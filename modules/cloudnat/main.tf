f525235f3ffe1aecadc801f3562649838e59c049
gayanswe <gayan@4trz.net>
2026-01-01 09:11:52 -0500

    Update modules/cloudnat/main.tf

--- modules/cloudnat/main.tf
+++ modules/cloudnat/main.tf
@@ -1,20 +1,20 @@
 resource "google_compute_router" "router" {
-  name    = "router"
+  name    = "${var.environment}-newproject-router-us-east4"
   network = var.network_name
   region  = var.region
   project = var.project_id
 }
 
+
 resource "google_compute_router_nat" "nat" {
-  name                          = "nat-config"
+  name                          = "${var.environment}-newproject-cloudnat-us-east4"
   router                        = google_compute_router.router.name
   region                        = var.region
-  project                       = var.project_id
-  nat_ip_allocate_option        = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  project = var.project_id
 
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
   subnetwork {
     name = var.subnet_name
-    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+    source_ip_ranges_to_nat = "ALL_IP_RANGES"
   }
 }
