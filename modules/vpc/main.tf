25fac0990b5a2ff432fbb8e15c7b1a15067802fa
gayanswe <gayan@4trz.net>
2026-01-01 05:16:11 -0500

    Update modules/vpc/main.tf

---
+++ modules/vpc/main.tf
@@ -0,0 +1,13 @@
+resource "google_compute_network" "vpc" {
+  name                    = var.vpc_name
+  project                 = var.project_id
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = var.subnet_name
+  ip_cidr_range = var.subnet_cidr
+  network       = google_compute_network.vpc.id
+  project       = var.project_id
+  region        = var.region
+}
