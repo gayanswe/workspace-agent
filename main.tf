01e4b9cccde2deee283dafaedc5aab883a2343b9
gayanswe <gayan@4trz.net>
2026-01-01 09:11:49 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,30 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
 module "vpc" {
   source       = "./modules/vpc"
   project_id   = var.project_id
   region       = var.region
+  environment  = var.environment
   vpc_name     = var.vpc_name
-  subnet_name  = var.subnet_name
   subnet_cidr  = var.subnet_cidr
+  subnet_name  = var.subnet_name
 }
 
 module "firewall" {
   source      = "./modules/firewall"
   project_id  = var.project_id
-  network_name = module.vpc.network_name #Reference the output from the VPC module
+  network_name = module.vpc.network_name
+  environment = var.environment
 }
 
 module "cloudnat" {
-  source         = "./modules/cloudnat"
-  project_id     = var.project_id
-  region         = var.region
-  network_name   = module.vpc.network_name
-  subnet_name = var.subnet_name
+  source          = "./modules/cloudnat"
+  project_id      = var.project_id
+  region          = var.region
+  environment     = var.environment
+  subnet_name     = module.vpc.subnet_name
+  network_name    = module.vpc.network_name
 }
