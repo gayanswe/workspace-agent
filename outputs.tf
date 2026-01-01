e16a46456ccddca0d664c7c63253d8969e3ca3b4
gayanswe <gayan@4trz.net>
2026-01-01 09:11:50 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
-output "vpc_id" {
-  value = module.vpc.network_id
+output "vpc_name" {
+  description = "The name of the VPC"
+  value       = module.vpc.network_name
+}
+
+output "subnet_name" {
+  description = "The name of the subnet"
+  value       = module.vpc.subnet_name
 }
