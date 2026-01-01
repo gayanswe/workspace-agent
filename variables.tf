2e41ca8f79ad2636834603e8d9636f0800000a5a
gayanswe <gayan@4trz.net>
2026-01-01 09:11:50 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 variable "project_id" {
   description = "GCP Project ID"
   type        = string
+  default     = "euphoric-effect-479410-a6" # Correct Project ID
 }
 
 variable "region" {
@@ -9,20 +10,26 @@ variable "region" {
   default     = "us-east4"
 }
 
-variable "vpc_name" {
-  description = "VPC Name"
+variable "environment" {
+  description = "Environment (e.g., dev, prod)"
   type        = string
-  default     = "dev-vpc"
+  default     = "dev"
 }
 
-variable "subnet_name" {
-  description = "Subnet Name"
+variable "vpc_name" {
+  description = "Name of the VPC"
   type        = string
-  default     = "dev-subnet"
+  default     = "dev-newproject-vpc-us-east4"
 }
 
 variable "subnet_cidr" {
-  description = "Subnet CIDR"
+  description = "CIDR block for the subnet"
   type        = string
   default     = "10.0.4.0/24"
+}
+
+variable "subnet_name" {
+  description = "Name of the subnet"
+  type        = string
+  default     = "dev-newproject-web-subnet-us-east4"
 }
