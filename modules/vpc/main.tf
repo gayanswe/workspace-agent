76ea78d605809b260617e39be3f4f864ac2b37af
gayanswe <gayan@4trz.net>
2026-01-01 09:11:51 -0500

    Update modules/vpc/main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -10,4 +10,14 @@ resource "google_compute_subnetwork" "subnet" {
   network       = google_compute_network.vpc.id
   project       = var.project_id
   region        = var.region
+
+  private_ip_google_access = true
+}
+
+output "network_name" {
+  value = google_compute_network.vpc.name
+}
+
+output "subnet_name" {
+  value = google_compute_subnetwork.subnet.name
 }
