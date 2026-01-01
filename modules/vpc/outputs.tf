feaf0628ab50895810f41719ee8092210eca45a0
gayanswe <gayan@4trz.net>
2026-01-01 05:16:12 -0500

    Update modules/vpc/outputs.tf

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,9 @@
+output "network_name" {
+  description = "The name of the VPC network"
+  value       = google_compute_network.vpc.name
+}
+
+output "network_id" {
+  description = "The ID of the VPC network"
+  value       = google_compute_network.vpc.id
+}
