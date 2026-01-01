749040681372f8be87b0df09409dab3e3eae4e4b
gayanswe <gayan@4trz.net>
2026-01-01 05:16:13 -0500

    Update modules/firewall/variables.tf

---
+++ modules/firewall/variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  type        = string
+  description = "The ID of the project"
+}
+
+variable "network_name" {
+  type        = string
+  description = "The name of the VPC network"
+}
