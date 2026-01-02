89ece243d60186df7d413bb0acabb309a768dde7
gayanswe <gayan@4trz.net>
2026-01-01 21:48:56 -0500

    Update modules/vpc/main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,19 +5,10 @@ resource "google_compute_network" "vpc" {
 }
 
 resource "google_compute_subnetwork" "subnet" {
-  name          = var.subnet_name
-  ip_cidr_range = var.subnet_cidr
-  network       = google_compute_network.vpc.id
-  project       = var.project_id
-  region        = var.region
-
+  name                     = var.subnet_name
+  project                  = var.project_id
+  ip_cidr_range            = var.subnet_cidr
+  region                   = var.region
+  network                  = google_compute_network.vpc.id
   private_ip_google_access = true
-}
-
-output "network_name" {
-  value = google_compute_network.vpc.name
-}
-
-output "subnet_name" {
-  value = google_compute_subnetwork.subnet.name
 }
