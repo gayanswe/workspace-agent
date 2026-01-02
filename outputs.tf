c994aabcf3a8fcb53850ba8b3b19bdf2b1912251
gayanswe <gayan@4trz.net>
2026-01-01 23:30:23 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,34 @@
-output "vpc_self_link" {
-  description = "The self_link of the created VPC network."
-  value       = module.vpc_network.vpc_self_link
+output "vpc_name" {
+  description = "The name of the created VPC."
+  value       = module.vpc_network.network_name
+}
+
+output "subnet_name" {
+  description = "The name of the created subnetwork."
+  value       = module.vpc_network.subnet_name
 }
 
 output "subnet_self_link" {
-  description = "The self_link of the created private subnetwork."
+  description = "The self_link of the created subnetwork."
   value       = module.vpc_network.subnet_self_link
 }
 
-output "firewall_ssh_rule_name" {
-  description = "The name of the firewall rule allowing SSH from GitHub CI/CD."
-  value       = module.firewall_rules.firewall_ssh_rule_name
+output "router_name" {
+  description = "The name of the Cloud Router."
+  value       = module.vpc_network.router_name
 }
 
-output "firewall_egress_rule_name" {
-  description = "The name of the firewall rule allowing all egress traffic."
-  value       = module.firewall_rules.firewall_egress_rule_name
+output "nat_name" {
+  description = "The name of the Cloud NAT gateway."
+  value       = module.vpc_network.nat_name
+}
+
+output "allow_ssh_firewall_rule_name" {
+  description = "The name of the SSH ingress firewall rule."
+  value       = module.firewall_rules.allow_ssh_rule_name
+}
+
+output "allow_egress_all_firewall_rule_name" {
+  description = "The name of the all egress firewall rule."
+  value       = module.firewall_rules.allow_egress_all_rule_name
 }
