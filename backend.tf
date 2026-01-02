be046805f2455c84a741e3baae457a83844f8c0a
gayanswe <gayan@4trz.net>
2026-01-02 11:18:08 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "gs://euphoric-effect-479410-a6-tfstate" # Replace with your bucket name
+    bucket = "tf-state-euphoric-effect-479410-a6" # This GCS bucket needs to be created manually once.
     prefix = "terraform/state"
   }
 }
