7aaea84029a125572d866a812e0ba99afa92c110
gayanswe <gayan@4trz.net>
2026-01-01 21:48:55 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,30 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.0.0"
+    }
+  }
+}
+
 provider "google" {
   project = var.project_id
   region  = var.region
 }
 
 module "vpc" {
-  source       = "./modules/vpc"
-  project_id   = var.project_id
-  region       = var.region
-  environment  = var.environment
-  vpc_name     = var.vpc_name
-  subnet_cidr  = var.subnet_cidr
-  subnet_name  = var.subnet_name
+  source     = "./modules/vpc"
+  project_id = var.project_id
+  region     = var.region
+  vpc_name   = var.vpc_name
+  subnet_name = var.subnet_name
+  subnet_cidr = var.subnet_cidr
 }
 
 module "firewall" {
-  source      = "./modules/firewall"
-  project_id  = var.project_id
+  source     = "./modules/firewall"
+  project_id = var.project_id
   network_name = module.vpc.network_name
-  environment = var.environment
-}
-
-module "cloudnat" {
-  source          = "./modules/cloudnat"
-  project_id      = var.project_id
-  region          = var.region
-  environment     = var.environment
-  subnet_name     = module.vpc.subnet_name
-  network_name    = module.vpc.network_name
+  allow_ssh_from = var.allow_ssh_from
 }
