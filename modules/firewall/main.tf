10fa1de6107e4a67dc7d9a827416dc2e7d5eda5c
gayanswe <gayan@4trz.net>
2026-01-01 23:12:55 -0500

    Update modules/firewall/main.tf

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -1,43 +1,43 @@
 resource "google_compute_firewall" "allow_ssh_github" {
-  project = var.project_id
-  name    = "${var.environment}-${var.project_name_prefix}-allow-ssh-github"
-  network = var.vpc_network_name
-  priority = var.firewall_priority
+  project     = var.project_id
+  name        = var.firewall_ssh_name
+  network     = var.vpc_network_name
+  description = "Allow SSH ingress from GitHub CI/CD source ranges."
+
+  direction     = "INGRESS"
+  priority      = 1000 # Default priority, can be adjusted
+  source_ranges = var.github_ci_cd_source_ranges
 
   allow {
     protocol = "tcp"
     ports    = ["22"]
   }
 
-  source_ranges = var.ssh_ingress_source_ranges
-  target_tags   = var.ssh_target_tags
-
-  description = "Allow SSH (TCP/22) from GitHub CI/CD source ranges to instances with '${join(",", var.ssh_target_tags)}' tag."
-
   labels = {
     environment = var.environment
-    project     = var.project_name_prefix
+    project     = var.project_name
     managed_by  = "terraform"
     created_by  = "opscontinuum"
   }
 }
 
 resource "google_compute_firewall" "allow_egress_all" {
-  project = var.project_id
-  name    = "${var.environment}-${var.project_name_prefix}-allow-egress"
-  network = var.vpc_network_name
-  priority = var.firewall_priority
+  project     = var.project_id
+  name        = var.firewall_egress_name
+  network     = var.vpc_network_name
+  description = "Allow all egress traffic from instances in the VPC."
+
+  direction        = "EGRESS"
+  priority         = 1000 # Default priority, can be adjusted
+  destination_ranges = ["0.0.0.0/0"] # Allow egress to all destinations
 
   allow {
     protocol = "all"
   }
 
-  destination_ranges = ["0.0.0.0/0"] # Egress to anywhere
-  description = "Allow all egress traffic from instances within the VPC."
-
   labels = {
     environment = var.environment
-    project     = var.project_name_prefix
+    project     = var.project_name
     managed_by  = "terraform"
     created_by  = "opscontinuum"
   }
