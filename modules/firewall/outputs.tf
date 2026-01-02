44a1e287ec2e6717b0790c32a58f9979ce2e387d
gayanswe <gayan@4trz.net>
2026-01-01 22:54:15 -0500

    Update modules/firewall/outputs.tf

--- modules/firewall/outputs.tf
+++ modules/firewall/outputs.tf
@@ -1,7 +1,9 @@
-output "allow_ssh_id" {
-  value = google_compute_firewall.allow_ssh.id
+output "ssh_firewall_rule_name" {
+  description = "Name of the SSH ingress firewall rule."
+  value       = google_compute_firewall.allow_ssh_github.name
 }
 
-output "allow_egress_id" {
-  value = google_compute_firewall.allow_egress.id
+output "egress_firewall_rule_name" {
+  description = "Name of the egress all firewall rule."
+  value       = google_compute_firewall.allow_egress_all.name
 }
