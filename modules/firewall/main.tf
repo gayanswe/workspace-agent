0fbe8a37068ac7701d10d5e830d4a4f036d49009
gayanswe <gayan@4trz.net>
2026-01-01 22:54:14 -0500

    Update modules/firewall/main.tf

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -1,26 +1,44 @@
-resource "google_compute_firewall" "allow_ssh" {
-  name    = "new-project-allow-ssh"
+resource "google_compute_firewall" "allow_ssh_github" {
   project = var.project_id
-  network = var.network_name
+  name    = "${var.environment}-${var.project_name_prefix}-allow-ssh-github"
+  network = var.vpc_network_name
+  priority = var.firewall_priority
 
   allow {
     protocol = "tcp"
     ports    = ["22"]
   }
 
-  source_ranges = [var.allow_ssh_from]
-  target_tags   = ["ssh"]
+  source_ranges = var.ssh_ingress_source_ranges
+  target_tags   = var.ssh_target_tags
+
+  description = "Allow SSH (TCP/22) from GitHub CI/CD source ranges to instances with '${join(",", var.ssh_target_tags)}' tag."
+
+  labels = {
+    environment = var.environment
+    project     = var.project_name_prefix
+    managed_by  = "terraform"
+    created_by  = "opscontinuum"
+  }
 }
 
-resource "google_compute_firewall" "allow_egress" {
-  name    = "new-project-allow-egress"
+resource "google_compute_firewall" "allow_egress_all" {
   project = var.project_id
-  network = var.network_name
+  name    = "${var.environment}-${var.project_name_prefix}-allow-egress"
+  network = var.vpc_network_name
+  priority = var.firewall_priority
 
   allow {
     protocol = "all"
   }
 
-  direction     = "EGRESS"
-  destination_ranges = ["0.0.0.0/0"]
+  destination_ranges = ["0.0.0.0/0"] # Egress to anywhere
+  description = "Allow all egress traffic from instances within the VPC."
+
+  labels = {
+    environment = var.environment
+    project     = var.project_name_prefix
+    managed_by  = "terraform"
+    created_by  = "opscontinuum"
+  }
 }
