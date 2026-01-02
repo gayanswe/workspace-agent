4c50c6c1c9c2d08bc4ad4f3658111e68f3cf7cf9
gayanswe <gayan@4trz.net>
2026-01-01 23:12:53 -0500

    Update modules/vpc/main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,27 +1,31 @@
-resource "google_compute_network" "custom_vpc" {
+resource "google_compute_network" "vpc_network" {
   project                 = var.project_id
-  name                    = "${var.environment}-${var.project_name_prefix}-vpc-${var.region}"
-  auto_create_subnetworks = false
+  name                    = var.vpc_name
+  auto_create_subnetworks = false # As per architecture, one custom subnet will be created
+  routing_mode            = "REGIONAL" # Best practice for regional deployments
+  mtu                     = 1460 # Default MTU for GCP
+  description             = "VPC network for ${var.environment} environment of ${var.project_name} project."
 
   labels = {
     environment = var.environment
-    project     = var.project_name_prefix
+    project     = var.project_name
     managed_by  = "terraform"
     created_by  = "opscontinuum"
   }
 }
 
 resource "google_compute_subnetwork" "private_subnet" {
-  project                  = var.project_id
-  name                     = "${var.environment}-${var.project_name_prefix}-subnet-${var.region}"
-  ip_cidr_range            = var.subnet_ip_cidr_range
-  region                   = var.region
-  network                  = google_compute_network.custom_vpc.self_link
-  private_ip_google_access = true # As requested, enable Private Google Access for the subnet.
+  project                    = var.project_id
+  name                       = var.subnet_name
+  ip_cidr_range              = var.subnet_cidr_range
+  network                    = google_compute_network.vpc_network.self_link
+  region                     = var.region
+  private_ip_google_access   = true # Enabled as per architecture
+  description                = "Private subnetwork for ${var.environment} environment."
 
   labels = {
     environment = var.environment
-    project     = var.project_name_prefix
+    project     = var.project_name
     managed_by  = "terraform"
     created_by  = "opscontinuum"
   }
