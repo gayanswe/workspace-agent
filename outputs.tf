9a633331c5abd7f087da909cc034291fcaaf4f40
gayanswe <gayan@4trz.net>
2026-01-02 14:41:22 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -18,7 +18,17 @@ output "subnet_self_link" {
   value       = google_compute_subnetwork.subnet.self_link
 }
 
-output "cloud_nat_name" {
-  description = "The name of the Cloud NAT gateway"
-  value       = google_compute_router_nat.nat.name
+output "nat_router_name" {
+  description = "Name of the Cloud Router provisioned for NAT"
+  value       = google_compute_router.router.name
+}
+
+output "nat_id" {
+  description = "The ID of the Cloud NAT gateway"
+  value       = google_compute_router_nat.nat.id
+}
+
+output "nat_self_link" {
+  description = "The self_link of the Cloud NAT gateway"
+  value       = google_compute_router_nat.nat.self_link
 }
