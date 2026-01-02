e21968c840d95aaf964820caa3f0f71e05fb47fa
gayanswe <gayan@4trz.net>
2026-01-02 08:09:18 -0500

    Update modules/firewall/outputs.tf

--- modules/firewall/outputs.tf
+++ modules/firewall/outputs.tf
@@ -1,9 +1,9 @@
-output "allow_ssh_rule_name" {
-  description = "The name of the firewall rule allowing SSH."
-  value       = google_compute_firewall.allow_ssh.name
+output "allow_ssh_ingress_name" {
+  description = "The name of the SSH ingress firewall rule."
+  value       = google_compute_firewall.allow_ssh_ingress.name
 }
 
-output "allow_egress_rule_name" {
-  description = "The name of the firewall rule allowing all egress."
-  value       = google_compute_firewall.allow_egress.name
+output "allow_egress_all_name" {
+  description = "The name of the all egress firewall rule."
+  value       = google_compute_firewall.allow_egress_all.name
 }
