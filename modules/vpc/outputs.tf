3e2b4826792986d894685d99d2bb6d1474d4caec
gayanswe <gayan@4trz.net>
2026-01-02 08:56:15 -0500

    Update modules/vpc/outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,19 +1,19 @@
 output "network_name" {
-  description = "The name of the created VPC network."
+  description = "The name of the custom VPC network."
   value       = google_compute_network.custom_vpc.name
 }
 
 output "network_self_link" {
-  description = "The self_link of the created VPC network."
+  description = "The self_link of the custom VPC network."
   value       = google_compute_network.custom_vpc.self_link
 }
 
 output "subnet_name" {
-  description = "The name of the created subnetwork."
+  description = "The name of the custom subnetwork."
   value       = google_compute_subnetwork.subnet_primary.name
 }
 
 output "subnet_id" {
-  description = "The ID of the created subnetwork."
+  description = "The ID of the custom subnetwork."
   value       = google_compute_subnetwork.subnet_primary.id
 }
