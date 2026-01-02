9f3d4a37391a9dfa119ad71badb787f3afb298c2
gayanswe <gayan@4trz.net>
2026-01-02 07:45:58 -0500

    Update modules/vpc/outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,29 +1,19 @@
-output "network_name" {
+output "vpc_name" {
   description = "The name of the created VPC network."
   value       = google_compute_network.vpc.name
 }
 
-output "network_self_link" {
+output "vpc_self_link" {
   description = "The self_link of the created VPC network."
   value       = google_compute_network.vpc.self_link
 }
 
 output "subnet_name" {
-  description = "The name of the created subnetwork."
+  description = "The name of the created private subnetwork."
   value       = google_compute_subnetwork.private.name
 }
 
 output "subnet_self_link" {
-  description = "The self_link of the created subnetwork."
+  description = "The self_link of the created private subnetwork."
   value       = google_compute_subnetwork.private.self_link
-}
-
-output "router_name" {
-  description = "The name of the Cloud Router."
-  value       = google_compute_router.router.name
-}
-
-output "nat_name" {
-  description = "The name of the Cloud NAT gateway."
-  value       = google_compute_router_nat.nat_gateway.name
 }
