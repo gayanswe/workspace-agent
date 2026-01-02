88702fdc79dbf9685d266f05622fd46b958e0542
gayanswe <gayan@4trz.net>
2026-01-02 08:54:44 -0500

    Update modules/vpc/main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,8 +1,8 @@
-resource "google_compute_network" "vpc" {
+resource "google_compute_network" "custom_vpc" {
+  name                    = "${var.environment}-${var.project_name_prefix}-${var.vpc_network_name}-${var.region}"
   project                 = var.project_id
-  name                    = "${var.environment}-${var.project_name_prefix}-vpc-${var.region}"
   auto_create_subnetworks = false
-  routing_mode            = "REGIONAL" # Best practice for granular control and costs
+  routing_mode            = "REGIONAL" # Best practice for custom mode VPCs
 
   labels = {
     environment = var.environment
@@ -12,13 +12,13 @@ resource "google_compute_network" "vpc" {
   }
 }
 
-resource "google_compute_subnetwork" "private" {
-  project                 = var.project_id
-  name                    = "${var.environment}-${var.project_name_prefix}-private-subnet-${var.region}"
-  ip_cidr_range           = var.subnet_cidr_range
-  region                  = var.region
-  network                 = google_compute_network.vpc.self_link
-  private_ip_google_access = var.private_google_access # Enables Private Google Access for instances in this subnet
+resource "google_compute_subnetwork" "subnet_primary" {
+  name                     = "${var.environment}-${var.project_name_prefix}-subnet-${var.region}"
+  project                  = var.project_id
+  ip_cidr_range            = var.subnet_cidr_block
+  region                   = var.region
+  network                  = google_compute_network.custom_vpc.self_link
+  private_ip_google_access = true # As per architecture
 
   labels = {
     environment = var.environment
