afc6374c36f62c0a7e09da7ebc201d7bfa20b624
gayanswe <gayan@4trz.net>
2026-01-02 11:45:22 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -11,25 +11,13 @@ variable "region" {
 }
 
 variable "vpc_name" {
-  description = "Name of the VPC. Follows naming convention: {env}-{project}-{resource}."
+  description = "Name of the VPC"
   type        = string
-  default     = "dev-new-project-vpc"
+  default     = "new-project-vpc"
 }
 
 variable "subnet_cidr" {
-  description = "CIDR range for the primary subnet (e.g., 10.0.4.0/24)."
+  description = "CIDR range for the private subnet"
   type        = string
   default     = "10.0.4.0/24"
-}
-
-variable "environment" {
-  description = "The environment name (e.g., dev, staging, prod) for resource labeling."
-  type        = string
-  default     = "dev"
-}
-
-variable "project_name" {
-  description = "The logical project name (e.g., new-project) for resource labeling."
-  type        = string
-  default     = "new-project"
 }
