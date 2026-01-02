fc1e66c65b39acc3caa4895991a8bf593338b76c
gayanswe <gayan@4trz.net>
2026-01-02 08:09:15 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,33 +3,25 @@ provider "google" {
   region  = var.region
 }
 
-module "vpc_network" {
-  source                = "./modules/vpc"
-  project_id            = var.project_id
-  region                = var.region
-  environment           = var.environment
-  project_name_prefix   = var.project_name_prefix
-  vpc_cidr_range        = var.vpc_cidr_range
-  subnet_cidr_range     = var.subnet_cidr_range
-  private_google_access = true # As per architecture: "VPC with Private Google Access"
+module "network" {
+  source = "./modules/network"
+
+  project_id             = var.project_id
+  region                 = var.region
+  environment            = var.environment
+  project_prefix         = var.project_prefix
+  vpc_cidr_block         = var.vpc_cidr_block
+  subnet_cidr_block      = var.subnet_cidr_block
+  subnet_name_suffix     = var.subnet_name_suffix
 }
 
-module "vpc_firewall" {
-  source              = "./modules/firewall"
-  project_id          = var.project_id
-  network_name        = module.vpc_network.vpc_name
-  environment         = var.environment
-  project_name_prefix = var.project_name_prefix
-  subnet_self_link    = module.vpc_network.subnet_self_link
-  ssh_source_ranges   = var.ssh_source_ranges
-}
+module "firewall" {
+  source = "./modules/firewall"
 
-module "vpc_cloudnat" {
-  source              = "./modules/cloudnat"
-  project_id          = var.project_id
-  region              = var.region
-  network_name        = module.vpc_network.vpc_name
-  subnet_self_link    = module.vpc_network.subnet_self_link
-  environment         = var.environment
-  project_name_prefix = var.project_name_prefix
+  project_id                 = var.project_id
+  environment                = var.environment
+  project_prefix             = var.project_prefix
+  network_name               = module.network.vpc_name # Pass the actual VPC name from the network module
+  subnet_cidr_block          = var.subnet_cidr_block
+  ssh_ingress_source_ranges  = var.ssh_ingress_source_ranges
 }
