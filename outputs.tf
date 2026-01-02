587f86a6eab821c4c1ff7f9429667d15ce674170
gayanswe <gayan@4trz.net>
2026-01-01 23:12:53 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,19 @@
 output "vpc_self_link" {
-  description = "Self link of the created VPC network."
-  value       = module.vpc.network_self_link
-}
-
-output "vpc_name" {
-  description = "Name of the created VPC network."
-  value       = module.vpc.network_name
+  description = "The self_link of the created VPC network."
+  value       = module.vpc_network.vpc_self_link
 }
 
 output "subnet_self_link" {
-  description = "Self link of the created subnet."
-  value       = module.vpc.subnet_self_link
+  description = "The self_link of the created private subnetwork."
+  value       = module.vpc_network.subnet_self_link
 }
 
-output "subnet_name" {
-  description = "Name of the created subnet."
-  value       = module.vpc.subnet_name
+output "firewall_ssh_rule_name" {
+  description = "The name of the firewall rule allowing SSH from GitHub CI/CD."
+  value       = module.firewall_rules.firewall_ssh_rule_name
 }
 
-output "nat_router_name" {
-  description = "Name of the Cloud Router used for NAT."
-  value       = module.nat.router_name
-}
-
-output "nat_gateway_name" {
-  description = "Name of the Cloud NAT gateway."
-  value       = module.nat.nat_name
+output "firewall_egress_rule_name" {
+  description = "The name of the firewall rule allowing all egress traffic."
+  value       = module.firewall_rules.firewall_egress_rule_name
 }
