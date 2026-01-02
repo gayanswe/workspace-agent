5685d6131a2bce9b6ace66d43911d58b6a9b24cf
gayanswe <gayan@4trz.net>
2026-01-02 07:45:57 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,19 @@
-output "vpc_self_link" {
-  description = "The self_link of the created VPC network."
-  value       = module.network.vpc_self_link
+output "vpc_name" {
+  description = "The name of the created VPC network."
+  value       = module.vpc_network.vpc_name
+}
+
+output "subnet_name" {
+  description = "The name of the created private subnetwork."
+  value       = module.vpc_network.subnet_name
 }
 
 output "subnet_self_link" {
-  description = "The self_link of the created private subnet."
-  value       = module.network.subnet_self_link
+  description = "The self_link of the created private subnetwork."
+  value       = module.vpc_network.subnet_self_link
 }
 
-output "subnet_ip_cidr_range" {
-  description = "The IP CIDR range of the created private subnet."
-  value       = module.network.subnet_ip_cidr_range
+output "cloud_nat_name" {
+  description = "The name of the Cloud NAT gateway."
+  value       = module.vpc_cloudnat.nat_gateway_name
 }
