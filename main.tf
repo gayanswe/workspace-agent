2b561eaae530b1463a9298cfe09bdc25425ebba1
gayanswe <gayan@4trz.net>
2026-01-01 23:43:31 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,22 +3,15 @@ provider "google" {
   region  = var.region
 }
 
-module "vpc_network" {
-  source = "./modules/vpc"
+module "network" {
+  source = "./modules/network"
 
-  project_id         = var.project_id
-  region             = var.region
-  environment        = var.environment
-  project_short_name = var.project_short_name
-  subnet_cidr        = var.subnet_cidr
-}
-
-module "firewall_rules" {
-  source = "./modules/firewall"
-
-  project_id                 = var.project_id
-  environment                = var.environment
-  project_short_name         = var.project_short_name
-  network_name               = module.vpc_network.network_name
-  client_ip_ssh_source_range = var.client_ip_ssh_source_range
+  project_id               = var.project_id
+  region                   = var.region
+  environment              = var.environment
+  project_name             = var.project_name
+  vpc_name                 = "${var.environment}-${var.project_name}-vpc-${var.region}"
+  subnet_name              = "${var.environment}-${var.project_name}-private-subnet-${var.region}"
+  subnet_cidr              = var.subnet_cidr
+  github_actions_ip_ranges = var.github_actions_ip_ranges
 }
