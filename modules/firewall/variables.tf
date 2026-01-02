f40f8b90cea4d413bf61e7f7bb8502dfe9f404e7
gayanswe <gayan@4trz.net>
2026-01-02 08:09:18 -0500

    Update modules/firewall/variables.tf

--- modules/firewall/variables.tf
+++ modules/firewall/variables.tf
@@ -3,29 +3,27 @@ variable "project_id" {
   type        = string
 }
 
+variable "environment" {
+  description = "The deployment environment (e.g., 'dev', 'prod'). Used for naming conventions."
+  type        = string
+}
+
+variable "project_prefix" {
+  description = "A short prefix for project resources, used in naming conventions."
+  type        = string
+}
+
 variable "network_name" {
   description = "The name of the VPC network to apply firewall rules to."
   type        = string
 }
 
-variable "environment" {
-  description = "The deployment environment."
+variable "subnet_cidr_block" {
+  description = "The CIDR range of the subnetwork (used for context or potential future rule refinement)."
   type        = string
 }
 
-variable "project_name_prefix" {
-  description = "A short prefix for resource naming."
-  type        = string
-}
-
-variable "ssh_source_ranges" {
-  description = "List of CIDR blocks from which SSH access (TCP:22) is allowed."
+variable "ssh_ingress_source_ranges" {
+  description = "List of CIDR ranges allowed to ingress TCP/22 (SSH)."
   type        = list(string)
-}
-
-variable "subnet_self_link" {
-  description = "Self link of the subnet for context, though not directly used in firewall rules for source/destination."
-  type        = string
-  # Added for completeness, but not directly used in rule definition.
-  # The subnet CIDR is implied for instances within it, which are targeted by tags.
 }
