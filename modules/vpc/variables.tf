4dc0ca31942dcf4978f663462d6d4e34c9880926
gayanswe <gayan@4trz.net>
2026-01-02 09:04:10 -0500

    Update modules/vpc/variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,11 +1,11 @@
 variable "project_id" {
-  description = "The GCP project ID to deploy resources into."
+  description = "The GCP project ID where the VPC will be created."
   type        = string
   default     = "euphoric-effect-479410-a6"
 }
 
 variable "region" {
-  description = "The GCP region for the network resources."
+  description = "The GCP region for the VPC and subnet."
   type        = string
   default     = "us-east4"
 }
@@ -23,13 +23,13 @@ variable "project_name_prefix" {
 }
 
 variable "vpc_network_name" {
-  description = "The name for the custom VPC network."
+  description = "The base name for the custom VPC network."
   type        = string
   default     = "vpc"
 }
 
 variable "subnet_cidr_block" {
-  description = "The CIDR block for the subnet."
+  description = "The CIDR block for the primary subnet within the VPC."
   type        = string
   default     = "10.0.4.0/24"
 }
