04789a0f7708c22ece9d7b0dfad4ba401a6f9c80
gayanswe <gayan@4trz.net>
2026-01-01 22:54:12 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,29 @@
-output "vpc_id" {
-  value = module.vpc.network_id
-  description = "The ID of the VPC"
+output "vpc_self_link" {
+  description = "Self link of the created VPC network."
+  value       = module.vpc.network_self_link
 }
 
-output "subnet_id" {
-  value = module.vpc.subnet_id
-  description = "The ID of the subnet"
+output "vpc_name" {
+  description = "Name of the created VPC network."
+  value       = module.vpc.network_name
+}
+
+output "subnet_self_link" {
+  description = "Self link of the created subnet."
+  value       = module.vpc.subnet_self_link
+}
+
+output "subnet_name" {
+  description = "Name of the created subnet."
+  value       = module.vpc.subnet_name
+}
+
+output "nat_router_name" {
+  description = "Name of the Cloud Router used for NAT."
+  value       = module.nat.router_name
+}
+
+output "nat_gateway_name" {
+  description = "Name of the Cloud NAT gateway."
+  value       = module.nat.nat_name
 }
