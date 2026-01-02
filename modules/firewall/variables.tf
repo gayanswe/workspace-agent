05b0fe81975f859269bf41aa37b7acb90402fb12
gayanswe <gayan@4trz.net>
2026-01-01 23:30:25 -0500

    Update modules/firewall/variables.tf

--- modules/firewall/variables.tf
+++ modules/firewall/variables.tf
@@ -4,31 +4,21 @@ variable "project_id" {
 }
 
 variable "environment" {
-  description = "The environment (e.g., dev, stage, prod)."
+  description = "Deployment environment (e.g., dev, prod, staging)."
   type        = string
 }
 
-variable "project_name" {
-  description = "A short name for the project, used in resource naming."
+variable "project_short_name" {
+  description = "Short name for the project, used in resource naming conventions."
   type        = string
 }
 
-variable "vpc_network_name" {
+variable "network_name" {
   description = "The name of the VPC network to apply firewall rules to."
   type        = string
 }
 
-variable "firewall_ssh_name" {
-  description = "The name for the SSH ingress firewall rule."
-  type        = string
-}
-
-variable "firewall_egress_name" {
-  description = "The name for the all egress firewall rule."
-  type        = string
-}
-
-variable "github_ci_cd_source_ranges" {
-  description = "List of CIDR ranges for GitHub CI/CD to allow SSH access."
+variable "client_ip_ssh_source_range" {
+  description = "CIDR block(s) from which SSH access (port 22) is allowed."
   type        = list(string)
 }
