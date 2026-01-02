73be28bbf666a316eae6b0045e02c4e6b050a540
gayanswe <gayan@4trz.net>
2026-01-02 16:06:28 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -11,43 +11,25 @@ variable "region" {
 }
 
 variable "vpc_name" {
-  description = "Name of the VPC (e.g., dev-project-vpc-region)"
+  description = "Name of the VPC"
   type        = string
-  default     = "dev-newproject-vpc-us-east4"
+  default     = "main-vpc"
 }
 
-variable "subnet_name" {
-  description = "Name of the private subnet (e.g., dev-project-subnet-region)"
-  type        = string
-  default     = "dev-newproject-subnet-us-east4"
-}
-
-variable "subnet_cidr" {
+variable "private_subnet_cidr" {
   description = "CIDR range for the private subnet"
   type        = string
   default     = "10.0.4.0/24"
 }
 
-variable "ssh_firewall_name" {
-  description = "Name for the Ingress SSH firewall rule"
-  type        = string
-  default     = "dev-newproject-allow-ssh"
+variable "github_cicd_ips" {
+  description = "List of IP ranges for GitHub CI/CD to allow SSH access"
+  type        = list(string)
+  default     = ["192.30.252.0/22", "185.199.108.0/22", "140.82.112.0/20"]
 }
 
-variable "egress_firewall_name" {
-  description = "Name for the Egress All firewall rule"
+variable "vpc_cidr" {
+  description = "Overall CIDR block for the VPC, used for internal firewall rules."
   type        = string
-  default     = "dev-newproject-allow-egress"
-}
-
-variable "nat_router_name" {
-  description = "Name for the Cloud Router used by NAT"
-  type        = string
-  default     = "dev-newproject-router-us-east4"
-}
-
-variable "nat_name" {
-  description = "Name for the Cloud NAT gateway"
-  type        = string
-  default     = "dev-newproject-nat-us-east4"
+  default     = "10.0.0.0/16" # Zen's architecture specified this
 }
