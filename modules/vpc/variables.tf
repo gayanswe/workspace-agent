595e29b0e4908c7ad65ad9b9dc8de362ea968a2d
gayanswe <gayan@4trz.net>
2026-01-01 23:30:24 -0500

    Update modules/vpc/variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -4,36 +4,21 @@ variable "project_id" {
 }
 
 variable "region" {
-  description = "GCP Region for the VPC and subnet."
+  description = "GCP Region for resource deployment."
   type        = string
 }
 
 variable "environment" {
-  description = "The environment (e.g., dev, stage, prod)."
+  description = "Deployment environment (e.g., dev, prod, staging)."
   type        = string
 }
 
-variable "project_name" {
-  description = "A short name for the project, used in resource naming."
+variable "project_short_name" {
+  description = "Short name for the project, used in resource naming conventions."
   type        = string
 }
 
-variable "vpc_name" {
-  description = "The name of the VPC network to create."
-  type        = string
-}
-
-variable "vpc_cidr_range" {
-  description = "The CIDR range for the VPC network."
-  type        = string
-}
-
-variable "subnet_name" {
-  description = "The name of the private subnetwork to create."
-  type        = string
-}
-
-variable "subnet_cidr_range" {
-  description = "The CIDR range for the private subnetwork."
+variable "subnet_cidr" {
+  description = "CIDR block for the private subnetwork."
   type        = string
 }
