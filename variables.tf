95861e8bb8f6d4c332ff9da28e930ffc70b7d12b
gayanswe <gayan@4trz.net>
2026-01-02 20:35:13 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,31 +5,31 @@ variable "project_id" {
 }
 
 variable "region" {
-  description = "GCP Region"
+  description = "GCP Region for resource deployment"
   type        = string
   default     = "us-east4"
 }
 
 variable "vpc_name" {
-  description = "Name of the VPC. Will be used as a prefix for other resources. Made more explicit to avoid regional confusion."
+  description = "Name for the main VPC network"
   type        = string
-  default     = "new-project-main-vpc-us-east4" # Updated for explicit region in name
+  default     = "new-project-vpc"
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC. Note: GCP VPCs are global and do not have a primary CIDR block. This is mainly for documentation or internal firewall rules."
+  description = "The primary CIDR range for the VPC"
   type        = string
   default     = "10.0.0.0/16"
 }
 
-variable "subnet_cidr" {
-  description = "CIDR range for the private subnet"
+variable "private_subnet_cidr" {
+  description = "CIDR range for the private subnet within the VPC"
   type        = string
-  default     = "10.0.1.0/24" # Changed to match general subnet pattern
+  default     = "10.0.4.0/24"
 }
 
 variable "ssh_source_ranges" {
-  description = "List of CIDR blocks allowed to SSH into instances with 'ssh-enabled' tag."
+  description = "List of CIDR ranges that are allowed to SSH into instances (e.g., your office IP, CI/CD runner IPs)"
   type        = list(string)
-  default     = ["0.0.0.0/0"] # For broad access; restrict this in production.
+  default     = ["0.0.0.0/0"] # Broad access by default, restrict in production
 }
