2b7e4c83b0e0887f68033ce2c4d861dc3f498af1
gayanswe <gayan@4trz.net>
2026-01-02 21:01:55 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,29 @@
 output "vpc_id" {
-  description = "The ID of the created VPC network"
+  description = "The ID of the custom VPC"
   value       = google_compute_network.vpc.id
 }
 
 output "vpc_self_link" {
-  description = "The self_link of the created VPC network"
+  description = "The self_link of the custom VPC"
   value       = google_compute_network.vpc.self_link
 }
 
 output "private_subnet_id" {
-  description = "The ID of the created private subnet"
-  value       = google_compute_subnetwork.private_subnet.id
+  description = "The ID of the private subnet"
+  value       = google_compute_subnetwork.private.id
 }
 
 output "private_subnet_self_link" {
-  description = "The self_link of the created private subnet"
-  value       = google_compute_subnetwork.private_subnet.self_link
+  description = "The self_link of the private subnet"
+  value       = google_compute_subnetwork.private.self_link
 }
 
-output "router_name" {
-  description = "The name of the Cloud Router"
-  value       = google_compute_router.nat_router.name
+output "cloud_router_name" {
+  description = "The name of the Cloud Router for NAT"
+  value       = google_compute_router.router.name
 }
 
-output "nat_name" {
+output "cloud_nat_name" {
   description = "The name of the Cloud NAT gateway"
-  value       = google_compute_router_nat.cloud_nat.name
+  value       = google_compute_router_nat.nat.name
 }
